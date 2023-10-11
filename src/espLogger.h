a698df8f09b23a02a1e6609548bc919f6b4ee687
gemi254 <gemi254@192.168.56.1>
2023-10-11 13:17:04 +0300

    Added log mode external

--- src/espLogger.h
+++ src/espLogger.h
@@ -1,8 +1,9 @@
 #ifndef __ESPLOGGER_H__
 #define __ESPLOGGER_H__
 
-#define LOGGER_LOG_MODE_SERIAL (1)
-#define LOGGER_LOG_MODE_FILE   (2)
+#define LOGGER_LOG_MODE_SERIAL     (1)  //Log to serial port
+#define LOGGER_LOG_MODE_FILE       (2)  //Log to a spiffs file
+#define LOGGER_LOG_MODE_EXTERNAL   (3)  //Provide the _log_printf
 
 #ifndef LOGGER_LOG_MODE
   #define LOGGER_LOG_MODE     LOGGER_LOG_MODE_SERIAL
@@ -30,21 +31,28 @@
     #define _DEBUG_PORT log_file 
     #define _CHK_LOG_FILE if(!log_file) log_file = STORAGE.open(LOGGER_LOG_FILENAME, "a+")
     #define LOGGER_CLOSE_LOG() log_file.close();
+#elif LOGGER_LOG_MODE == LOGGER_LOG_MODE_EXTERNAL
+    #ifndef _log_printf
+    #endif 
 #endif
 
-#define log_printf(...) _DEBUG_PORT.printf(__VA_ARGS__)
+#if LOGGER_LOG_MODE != LOGGER_LOG_MODE_EXTERNAL
+#define _log_printf(...) _DEBUG_PORT.printf(__VA_ARGS__)
+#endif 
 
 #ifdef ESP32
     #define _LOG_FORMAT(type, format) "[%s %s @ %s:%u] " format "", esp_log_system_timestamp(), #type, pathToFileName(__FILE__), __LINE__
 #else
-    #define _LOG_FORMAT(format, type) "[%s %s %u] " format "",type, __FILE__, __LINE__ 
+    #define _LOG_FORMAT(type, format) "[%s @ %s:%u] " format "", #type, __FILE__, __LINE__
 #endif
 
 #if LOGGER_LOG_LEVEL >= _LOG_LEVEL_VERBOSE
     #if LOGGER_LOG_MODE == LOGGER_LOG_MODE_FILE  
-        #define LOG_V(format, ...) { _CHK_LOG_FILE;  log_printf(_LOG_FORMAT(V, format), ##__VA_ARGS__); }
+        #define LOG_V(format, ...) { _CHK_LOG_FILE;  _log_printf(_LOG_FORMAT(V, format), ##__VA_ARGS__); }
+    #elif LOGGER_LOG_MODE == LOGGER_LOG_MODE_EXTERNAL
+        #define LOG_V(format, ...) { _log_printf(_LOG_FORMAT(V, format), ##__VA_ARGS__); }
     #else
-        #define LOG_V(format, ...) log_printf(_LOG_FORMAT(V, format), ##__VA_ARGS__)
+        #define LOG_V(format, ...) _log_printf(_LOG_FORMAT(V, format), ##__VA_ARGS__)
     #endif
 #else
     #define LOG_V(format, ...)
@@ -52,9 +60,11 @@
 
 #if LOGGER_LOG_LEVEL >= _LOG_LEVEL_DEBUG
     #if LOGGER_LOG_MODE == LOGGER_LOG_MODE_FILE  
-        #define LOG_D(format, ...) { _CHK_LOG_FILE;  log_printf(_LOG_FORMAT(E, format), ##__VA_ARGS__); }
+        #define LOG_D(format, ...) { _CHK_LOG_FILE;  _log_printf(_LOG_FORMAT(E, format), ##__VA_ARGS__); }
+    #elif LOGGER_LOG_MODE == LOGGER_LOG_MODE_EXTERNAL
+        #define LOG_D(format, ...) { _log_printf(_LOG_FORMAT(D, format), ##__VA_ARGS__); }
     #else
-        #define LOG_D(format, ...) log_printf(_LOG_FORMAT(D, format), ##__VA_ARGS__)
+       #define LOG_D(format, ...) _log_printf(_LOG_FORMAT(D, format), ##__VA_ARGS__)
     #endif
 #else
     #define LOG_D(format, ...)
@@ -62,9 +72,11 @@
 
 #if LOGGER_LOG_LEVEL >= _LOG_LEVEL_INFO
     #if LOGGER_LOG_MODE == LOGGER_LOG_MODE_FILE   
-        #define LOG_I(format, ...) { _CHK_LOG_FILE;  log_printf(_LOG_FORMAT(E, format), ##__VA_ARGS__); }
+        #define LOG_I(format, ...) { _CHK_LOG_FILE;  _log_printf(_LOG_FORMAT(E, format), ##__VA_ARGS__); }
+    #elif LOGGER_LOG_MODE == LOGGER_LOG_MODE_EXTERNAL
+        #define LOG_I(format, ...) { _log_printf(_LOG_FORMAT(I, format), ##__VA_ARGS__); }
     #else
-        #define LOG_I(format, ...) log_printf(_LOG_FORMAT(I, format), ##__VA_ARGS__)
+        #define LOG_I(format, ...) _log_printf(_LOG_FORMAT(I, format), ##__VA_ARGS__)
     #endif
 #else
     #define LOG_I(format, ...)
@@ -72,9 +84,11 @@
 
 #if LOGGER_LOG_LEVEL >= _LOG_LEVEL_WARN
     #if LOGGER_LOG_MODE == LOGGER_LOG_MODE_FILE  
-        #define LOG_W(format, ...) { _CHK_LOG_FILE;  log_printf(_LOG_FORMAT(E, format), ##__VA_ARGS__); }
+        #define LOG_W(format, ...) { _CHK_LOG_FILE;  _log_printf(_LOG_FORMAT(E, format), ##__VA_ARGS__); }
+    #elif LOGGER_LOG_MODE == LOGGER_LOG_MODE_EXTERNAL
+        #define LOG_W(format, ...) { _log_printf(_LOG_FORMAT(W, format), ##__VA_ARGS__); }
     #else
-        #define LOG_W(format, ...) log_printf(_LOG_FORMAT(W, format), ##__VA_ARGS__)
+        #define LOG_W(format, ...) _log_printf(_LOG_FORMAT(W, format), ##__VA_ARGS__)
     #endif
 #else
     #define LOG_W(format, ...)
@@ -82,9 +96,11 @@
 
 #if LOGGER_LOG_LEVEL >= _LOG_LEVEL_ERROR
     #if LOGGER_LOG_MODE == LOGGER_LOG_MODE_FILE
-        #define LOG_E(format, ...) { _CHK_LOG_FILE;  log_printf(_LOG_FORMAT(E, format), ##__VA_ARGS__); }
+        #define LOG_E(format, ...) { _CHK_LOG_FILE;  _log_printf(_LOG_FORMAT(E, format), ##__VA_ARGS__); }
+    #elif LOGGER_LOG_MODE == LOGGER_LOG_MODE_EXTERNAL
+        #define LOG_E(format, ...) { _log_printf(_LOG_FORMAT(E, format), ##__VA_ARGS__); }
     #else
-        #define LOG_E(format, ...) log_printf(_LOG_FORMAT(E, format), ##__VA_ARGS__)
+        #define LOG_E(format, ...) _log_printf(_LOG_FORMAT(E, format), ##__VA_ARGS__)
     #endif
 #else
     #define LOG_E(format, ...)
@@ -92,9 +108,11 @@
 
 #if LOGGER_LOG_LEVEL >= _LOG_LEVEL_NONE
     #if LOGGER_LOG_MODE == LOGGER_LOG_MODE_FILE
-        #define LOG_N(format, ...) { _CHK_LOG_FILE;  log_printf(_LOG_FORMAT(E, format), ##__VA_ARGS__); }
+        #define LOG_N(format, ...) { _CHK_LOG_FILE;  _log_printf(_LOG_FORMAT(N, format), ##__VA_ARGS__); }
+    #elif LOGGER_LOG_MODE == LOGGER_LOG_MODE_EXTERNAL
+        #define LOG_N(format, ...) { _log_printf(_LOG_FORMAT(N, format), ##__VA_ARGS__); }
     #else
-        #define LOG_N(format, ...) log_printf(_LOG_FORMAT(E, format), ##__VA_ARGS__)
+        #define LOG_N(format, ...) _log_printf(_LOG_FORMAT(N, format), ##__VA_ARGS__)
     #endif    
 #else
     #define LOG_N(format, ...)
