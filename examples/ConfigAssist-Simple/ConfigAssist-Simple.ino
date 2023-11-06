3ec0b353a61dd48a77ad36256f8af097276f6e2d
gemi254 <gemi254@192.168.56.1>
2023-11-06 19:25:17 +0200

    Updated v2.7.1

--- examples/ConfigAssist-Simple/ConfigAssist-Simple.ino
+++ examples/ConfigAssist-Simple/ConfigAssist-Simple.ino
@@ -26,21 +26,20 @@ void setup() {
   // And json disabled
   ConfigAssist info("/info.ini", NULL);
   
-  //info.deleteConfig(); //Uncomment to remove ini file and re-built
+  //info.deleteConfig(); // Uncomment to remove ini file and re-built
   
-  //Add a key even if ini not exists. 
-  //It will be not editable, but it will be saved to an ini file
+  // Add a key even if ini not exists. 
+  // It will be not editable, but it will be saved to an ini file
   if(!info.valid()){
     info.put("var1", "test1", true);
     info.put("var2", 1234, true);
-    //Save keys & values into ini file
+    // Save keys & values into ini file
     info.saveConfigFile();
-  }else{ //Ini file is valid, display the values
+  }else{ // Ini file is valid, display the values
     LOG_I("Info file: var1:  %s, var2: %s\n", info["var1"].c_str(), info["var2"].c_str() );      
   }
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
 }
