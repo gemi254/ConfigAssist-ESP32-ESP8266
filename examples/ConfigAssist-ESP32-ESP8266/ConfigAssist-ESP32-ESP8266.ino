a0af486ab080ebee8e47dfaf2fd3514c722dd1f9
gemi254 <gemi254@192.168.56.1>
2023-11-06 19:24:34 +0200

    Append dump handler

--- examples/ConfigAssist-ESP32-ESP8266/ConfigAssist-ESP32-ESP8266.ino
+++ examples/ConfigAssist-ESP32-ESP8266/ConfigAssist-ESP32-ESP8266.ino
@@ -23,7 +23,7 @@
   ESP8266WebServer  server(80);
 #endif
 
-//Define application name
+// Define application name
 #define APP_NAME "ConfigAssistDemo"
 #define INI_FILE "/ConfigAssistDemo.ini"
 // Default application config dictionary
@@ -136,7 +136,7 @@ void debugMemory(const char* caller) {
 // List storage file system
 void ListDir(const char * dirname) {
   LOG_I("Listing directory: %s\n", dirname);
-  // ist details of files on file system
+  // List details of files on file system
   File root = STORAGE.open(dirname,"r");
   File file = root.openNextFile();
   while (file) {
@@ -194,7 +194,7 @@ void setup(void) {
   Serial.print("\n\n\n\n");
   Serial.flush();
   
-  //Start local storage
+  // Start local storage
   #if defined(ESP32)  
     if(!STORAGE.begin(true)) Serial.println("ESP32 storage init failed!"); 
   #else
@@ -205,16 +205,16 @@ void setup(void) {
   LOG_I("Starting..\n");
   debugMemory("setup");
 
-  //Uncomment to remove old ini file and re-built it fron dictionary
+  // Uncomment to remove old ini file and re-built it fron dictionary
   //conf.deleteConfig();
 
-  //Register handlers for web server    
+  // Register handlers for web server    
   server.on("/", handleRoot);
 
-  //Failed to load config or ssid empty
+  // Failed to load config or ssid empty
   if(conf["st_ssid"]=="" ){ 
-    //Start Access point server and edit config
-    //Data will be availble instantly 
+    // Start Access point server and edit config
+    // Data will be availble instantly 
     conf.setup(server, true);
     return;
   }
@@ -222,7 +222,7 @@ void setup(void) {
   debugMemory("Loaded config");
   pinMode(conf["led_pin"].toInt(), OUTPUT);
      
-  //Connect to Wifi station with ssid from conf file
+  // Connect to Wifi station with ssid from conf file
   uint32_t startAttemptTime = millis();
   WiFi.setAutoReconnect(false);
   WiFi.setAutoConnect(false);
@@ -239,51 +239,52 @@ void setup(void) {
   }  
   Serial.println();
   
-  //Check connection
+  // Check connection
   if(WiFi.status() == WL_CONNECTED ){
     LOG_I("Wifi AP SSID: %s connected, use 'http://%s' to connect\n", conf["st_ssid"].c_str(), WiFi.localIP().toString().c_str()); 
   }else{
-    //Fall back to Access point for editing config
+    // Fall back to Access point for editing config
     LOG_E("Connect failed.\n");
     conf.setup(server, true);
     return;
   }
   
   if (MDNS.begin(conf["host_name"].c_str())) {
-    LOG_I("MDNS responder started\n");
+    LOG_V("MDNS responder started\n");
   }
 
-  //Get int/bool value
+  // Get int/bool value
   bool debug = conf["debug"].toInt();
   LOG_I("Boolean value: %i\n", debug);  
   
-  //Get float value
+  // Get float value
   float float_value = atof(conf["float_val"].c_str());
   LOG_I("Float value: %1.5f\n", float_value);
   
-  //Change a value
+  // Change a value
   //conf.put("led_pin","3");
   
-  //Also change an int/bool value
+  // Also change an int/bool value
   //conf.put("led_pin", 4);
   
-  //Add handlers to web server 
+  // Add handlers to web server 
   conf.setup(server);
   
-  server.on("/inline", []() {
-    server.send(200, "text/plain", "this works as well");
-    conf.dump();           
+  // Append dump handler
+  server.on("/d", []() {
+    //server.send(200, "text/plain", "ConfigAssist dump");
+    conf.dump(server);
   });
-  
+
   server.onNotFound(handleNotFound);
   server.begin();
-  LOG_I("HTTP server started\n");
+  LOG_V("HTTP server started\n");
   
-  //On the fly generate an ini info file on SPIFFS
+  // On the fly generate an ini info file on SPIFFS
   {
     if(debug) STORAGE.remove("/info.ini");
     ConfigAssist info("/info.ini");
-    //Add a key even if not exists. It will be not editable
+    // Add a key even if not exists. It will be not editable
     if(!info.valid()){
       info.put("var1", "test1", true);
       info.put("var2", 1234, true);
@@ -300,13 +301,13 @@ void loop(void) {
     MDNS.update();
   #endif  
   
-  //Display info
+  // Display info
   if (millis() - pingMillis >= 10000){
-    //if debug is enabled in config display memory debug messages    
+    // if debug is enabled in config display memory debug messages    
     if(conf["debug"].toInt()) debugMemory("Loop");
     pingMillis = millis();
   } 
   
-  //allow the cpu to switch to other tasks  
+  // Allow the cpu to switch to other tasks  
   delay(2);
 }
