0f41a42130934d1057c090072a7bac8d46b6a585
gemi254 <gemi254@192.168.56.1>
2023-11-15 15:33:51 +0200

    Firmware 1.0.0

--- examples/ConfigAssist-FirmwareCheck/ConfigAssist-FirmwareCheck.ino
+++ examples/ConfigAssist-FirmwareCheck/ConfigAssist-FirmwareCheck.ino
@@ -18,80 +18,18 @@
 #include <ConfigAssist.h>  // Config assist class
 
 #if defined(ESP32)
+  #include "firmCheckESP32PMem.h"
   WebServer server(80);
 #else
+  #include "firmCheckESP8266PMem.h"
   ESP8266WebServer  server(80);
 #endif
 
 // Define application name
 #define APP_NAME "FirmwareCheck"
 #define INI_FILE "/FirmwareCheck.ini"
-char FIRMWARE_VERSION[] = "1.0.0";
-// Default application config dictionary
-// Modify the file with the params for you application
-// Then you can use then then by val = config[name];
-const char* VARIABLES_DEF_JSON PROGMEM = R"~(
-[{
-   "seperator": "Wifi settings"
-  },{
-    "name": "st_ssid",
-     "label": "Name for WLAN",
-   "default": ""
-  },{
-      "name": "st_pass",
-     "label": "Password for WLAN",
-   "default": ""
-  },{
-      "name": "host_name",
-     "label": "Host name to use for MDNS and AP<br>{mac} will be replaced with device's mac id",
-   "default": "configAssist_{mac}"
-  },{
- "seperator": "Application settings"
-  },{
-      "name": "app_name",
-     "label": "Name your application",
-   "default": "FirmwareCheck"
-  },{
-      "name": "firmware_url",
-     "label": "Firmware upgrade url with version and info",
-   "default": "https://raw.githubusercontent.com/gemi254/ConfigAssist-ESP32-ESP8266/main/examples/ConfigAssist-FirmwareCheck/firmware/lastest.json"
-  },{
-      "name": "time_zone",
-     "label": "Needs to be a valid time zone string",
-   "default": "EET-2EEST,M3.5.0/3,M10.5.0/4",    
-  "datalist": "
-'Etc/GMT,GMT0'
-'Etc/GMT-0,GMT0'
-'Etc/GMT-1,<+01>-1'
-'Etc/GMT-2,<+02>-2'
-'Etc/GMT-3,<+03>-3'
-'Etc/GMT-4,<+04>-4'
-'Etc/GMT-5,<+05>-5'
-'Etc/GMT-6,<+06>-6'
-'Etc/GMT-7,<+07>-7'
-'Etc/GMT-8,<+08>-8'
-'Etc/GMT-9,<+09>-9'
-'Etc/GMT-10,<+10>-10'
-'Etc/GMT-11,<+11>-11'
-'Etc/GMT-12,<+12>-12'
-'Etc/GMT-13,<+13>-13'
-'Etc/GMT-14,<+14>-14'
-'Etc/GMT0,GMT0'
-'Etc/GMT+0,GMT0'
-'Etc/GMT+1,<-01>1'
-'Etc/GMT+2,<-02>2'
-'Etc/GMT+3,<-03>3'
-'Etc/GMT+4,<-04>4'
-'Etc/GMT+5,<-05>5'
-'Etc/GMT+6,<-06>6'
-'Etc/GMT+7,<-07>7'
-'Etc/GMT+8,<-08>8'
-'Etc/GMT+9,<-09>9'
-'Etc/GMT+10,<-10>10'
-'Etc/GMT+11,<-11>11'
-'Etc/GMT+12,<-12>12'"  
-}
-])~"; 
+
+char FIRMWARE_VERSION[] = "1.0.0";    // Firmware version to check
 
 // Config class
 ConfigAssist conf(INI_FILE, VARIABLES_DEF_JSON);
