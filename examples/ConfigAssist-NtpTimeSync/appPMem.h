dd29d28d1de5110c71db707926d439126cb90c3f
gemi254 <gemi254@192.168.56.1>
2024-02-14 13:23:05 +0200

    Fixed led build in

--- examples/ConfigAssist-NtpTimeSync/appPMem.h
+++ examples/ConfigAssist-NtpTimeSync/appPMem.h
@@ -18,8 +18,8 @@ Application settings:
   - led_buildin:
       label: Enter the pin that the build in led is connected. 
         Leave blank for auto.
-      attribs: "min='4' max='23' step='1'"
-      default: 4
+      attribs: "min='2' max='23' step='1'"
+      default:
 
 Time settings:
   - time_zone:
