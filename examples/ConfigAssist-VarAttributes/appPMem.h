1ef3f414a2cc4e5584e53a6df2d80f787dd3229b
gemi254 <gemi254@192.168.56.1>
2024-02-14 13:23:46 +0200

    Added javascript INIT_SCRIPT

--- examples/ConfigAssist-VarAttributes/appPMem.h
+++ examples/ConfigAssist-VarAttributes/appPMem.h
@@ -2,10 +2,10 @@ const char* VARIABLES_DEF_YAML PROGMEM = R"~(
 Wifi settings:
   - st_ssid:
       label: Name for WLAN
-      default: mdk3
+      default:
   - st_pass:
       label: Password for WLAN
-      default: 2843028858
+      default:
   - host_name: 
       label: >-
         Host name to use for MDNS and AP<br>{mac} will be replaced with device's mac id
@@ -16,11 +16,10 @@ Application settings:
       label: Name your application
       default: VarAttribues  
   - led_buildin:
-      label: Enter the pin that the build in led is connected. 
-        Leave blank for auto.
-        Usually pin 4 on esp32, pin 2 on esp8266
-      attribs: "min='4' max='23' step='1'"
-      default: 4
+      label: Pin that the build in led is connected. Leave blank for auto.
+        Usually pin 4 on esp32, 2 on esp8266
+      attribs: "min='2' max='23' step='1'"
+      default:
   - display_style:
       label: Choose how the config sections are displayed. 
         Must reboot to apply
@@ -34,23 +33,14 @@ Batch upload settings:
   - batch_upload:
       label: "Enable batch upload measurements"
       checked: False
-      attribs: >-
-        onClick="
-        if(this.checked){
-          document.getElementById('batch_size').disabled = false;
-          document.getElementById('update_interval').disabled = false;
-        }else{
-          document.getElementById('batch_size').disabled = true;
-          document.getElementById('update_interval').disabled = true;
-        }"  
+      attribs: onClick="enableDisableBatchUpload()"  
   - batch_size:
       label: "Measurements size"
       default: 5
       attribs: >-
         onChange="const ctrl = document.getElementById('update_interval');
         const ui = ((ctrl.value * this.value) / 1000)/60 + ' mins';
-        this.parentElement.parentElement.childNodes[5].innerHTML = 'Upload every&#8282; ' +  ui;
-        "
+        this.parentElement.parentElement.childNodes[5].innerHTML = 'Upload every&#8282; ' +  ui;"
   - update_interval:
       label: "Delay between measurements (ms)"
       default: 30000
@@ -58,6 +48,20 @@ Batch upload settings:
         onChange="
           const ctrl = document.getElementById('batch_size');
           const ui = ((ctrl.value * this.value) / 1000)/60 + ' mins';
-          ctrl.parentElement.parentElement.childNodes[5].innerHTML = 'Upload every&#8282; ' +  ui;
-        " 
+          ctrl.parentElement.parentElement.childNodes[5].innerHTML = 'Upload every&#8282; ' +  ui;" 
 )~"; 
+
+PROGMEM const char INIT_SCRIPT[] = R"=====(
+function enableDisableBatchUpload(){
+  if($('#batch_upload').checked){
+    document.getElementById('batch_size').disabled = false;
+    document.getElementById('update_interval').disabled = false;
+  }else{
+    document.getElementById('batch_size').disabled = true;
+    document.getElementById('update_interval').disabled = true;
+  }
+}
+document.addEventListener('DOMContentLoaded', function (event) {
+  enableDisableBatchUpload();
+});  
+)=====";
