73b7901ecfe4a4e3f54616acb7f7ea6320d9cd29
gemi254 <gemi254@192.168.56.1>
2024-04-23 21:45:49 +0300

    Fixed  caclulate Upload interval

--- examples/ConfigAssist-VarAttributes/appPMem.h
+++ examples/ConfigAssist-VarAttributes/appPMem.h
@@ -6,7 +6,7 @@ Wifi settings:
   - st_pass:
       label: Password for WLAN
       default:
-  - host_name: 
+  - host_name:
       label: >-
         Host name to use for MDNS and AP<br>{mac} will be replaced with device's mac id
       default: ConfigAssist_{mac}
@@ -14,42 +14,43 @@ Wifi settings:
 Application settings:
   - app_name:
       label: Name your application
-      default: VarAttribues  
+      default: VarAttribues
   - led_buildin:
       label: Pin that the build in led is connected. Leave blank for auto.
         Usually pin 4 on esp32, 2 on esp8266
       attribs: "min='2' max='23' step='1'"
       default:
   - display_style:
-      label: Choose how the config sections are displayed. 
+      label: Choose how the config sections are displayed.
         Must reboot to apply
       options: AllOpen: 0
-        - AllClosed: 1 
+        - AllClosed: 1
         - Accordion : 2
         - AccordionToggleClosed : 3
-      default: AccordionToggleClosed   
+      default: AccordionToggleClosed
 
 Batch upload settings:
   - batch_upload:
-      label: "Enable batch upload measurements"
-      checked: False
-      attribs: onClick="enableDisableBatchUpload()"  
+      label: >-
+        Enable batch upload measurements
+        <b>Upload interval &#8282;</b> <span id='uploadInterval'>2.5 mins</span>
+      checked: True
+      attribs: onClick="enableDisableBatchUpload()"
   - batch_size:
-      label: "Measurements size"
+      label: "Measurements size. Change to calculate <b>Upload interval</b>"
       default: 5
       attribs: >-
         onChange="const ctrl = document.getElementById('update_interval');
-        const ui = ((ctrl.value * this.value) / 1000)/60 + ' mins';
-        this.parentElement.parentElement.childNodes[5].innerHTML = 'Upload every&#8282; ' +  ui;"
+        const ui = ((ctrl.value * this.value) / 1000) / 60;
+        document.getElementById('uploadInterval').innerHTML = ui.toFixed(2) + ' mins';"
   - update_interval:
-      label: "Delay between measurements (ms)"
+      label: "Delay between measurements (ms).Change to calculate <b>Upload interval</b>"
       default: 30000
       attribs: >-
-        onChange="
-          const ctrl = document.getElementById('batch_size');
-          const ui = ((ctrl.value * this.value) / 1000)/60 + ' mins';
-          ctrl.parentElement.parentElement.childNodes[5].innerHTML = 'Upload every&#8282; ' +  ui;" 
-)~"; 
+        onChange="const ctrl = document.getElementById('batch_size');
+        const ui = ((ctrl.value * this.value) / 1000) / 60;
+        document.getElementById('uploadInterval').innerHTML = ui.toFixed(2) + ' mins';"
+)~";
 
 PROGMEM const char INIT_SCRIPT[] = R"=====(
 function enableDisableBatchUpload(){
@@ -63,5 +64,5 @@ function enableDisableBatchUpload(){
 }
 document.addEventListener('DOMContentLoaded', function (event) {
   enableDisableBatchUpload();
-});  
+});
 )=====";
