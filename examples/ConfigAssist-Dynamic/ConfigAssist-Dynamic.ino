a60154179e7bb23f0238100f4bb6014e5921c735
gemi254 <gemi254@192.168.56.1>
2024-04-23 21:44:12 +0300

    Fixed errors

--- examples/ConfigAssist-Dynamic/ConfigAssist-Dynamic.ino
+++ examples/ConfigAssist-Dynamic/ConfigAssist-Dynamic.ino
@@ -1,36 +1,42 @@
 #include <ConfigAssist.h>  // Config assist class definition
 
-// Create a config class with an ini filename for storage 
+// Create a config class with an ini filename for storage
 ConfigAssist config("/info1.ini");
 
 void setup() {
   // put your setup code here, to run once:
-    
+
   Serial.begin(115200);
   Serial.print("\n\n\n\n");
   Serial.flush();
 
+  #if defined(ESP32)
+    if(!STORAGE.begin(true)) Serial.println("ESP32 storage init failed!");
+  #else
+    if(!STORAGE.begin()) Serial.println("ESP8266 storage init failed!");
+  #endif
+
   LOG_I("Starting..\n");
-  
-  //config.deleteConfig(); // Uncomment to remove ini file and re-built   
-  
+  LOG_I("Config exists: %i\n",config.confExists());
+  //Start storage and builds config
+  //config.deleteConfig(); // Uncomment to remove ini file and re-built
+
   // Dict is disabled, Ini file not found
-  if(!config.valid()){
+  if(!config.confExists()){
     String textDict="YAML DICT: \n";
     for(int i=0; i<10; ++i){
       textDict += "  - var_"+ String(i) +":\n";
-      textDict += "     default: "+ String(i) +"\n";
+      textDict += "      default: "+ String(i) +"\n";
     }
-    LOG_I("Generated yaml: %s\n",textDict.c_str());
-   
+    LOG_I("Generated yaml:\n %s\n",textDict.c_str());
+
     // Build ini file from textDict
-    config.setDictStr(textDict.c_str(),true);
+    config.setDictStr(textDict.c_str(), true);
     LOG_I("Config valid: %i\n",config.valid());
-    config.dump();
     // Save keys & values into ini file
     config.saveConfigFile();
+    config.dump();
   }else{ // Ini file is valid, display the values
-    LOG_I("Config valid: %i\n",config.valid());
     config.dump();
   }
 }
