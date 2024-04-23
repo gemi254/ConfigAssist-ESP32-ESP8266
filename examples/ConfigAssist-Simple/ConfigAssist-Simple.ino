e07ecba48f99cf2416c340059fa2969be440a0c1
gemi254 <gemi254@192.168.56.1>
2024-04-23 21:48:36 +0300

    Added confExists

--- examples/ConfigAssist-Simple/ConfigAssist-Simple.ino
+++ examples/ConfigAssist-Simple/ConfigAssist-Simple.ino
@@ -2,34 +2,36 @@
 
 void setup() {
   // put your setup code here, to run once:
-    
+
   Serial.begin(115200);
   Serial.print("\n\n\n\n");
   Serial.flush();
 
-  #if defined(ESP32)  
-    if(!STORAGE.begin(true)) Serial.println("ESP32 storage init failed!"); 
+  #if defined(ESP32)
+    if(!STORAGE.begin(true)) Serial.println("ESP32 storage init failed!");
   #else
-    if(!STORAGE.begin()) Serial.println("ESP8266 storage init failed!"); 
+    if(!STORAGE.begin()) Serial.println("ESP8266 storage init failed!");
   #endif
   LOG_I("Starting..\n");
-  
+
   // Create a config class with an ini filename for storage
   // and disabled json (NULL)
   ConfigAssist info("/info.ini", NULL);
-  
+
   //info.deleteConfig(); // Uncomment to remove ini file and re-built
-  
+
   // Ini file not exists
-  if(!info.valid()) {
-    // Add a key even if not exists. 
+  if(!info.confExists()) {
+    // Add a key even if not exists.
     // It will be not editable, but it will be saved to an ini file
+    LOG_I("Config file not exists\n");
     info.put("var1", "test1", true);
     info.put("var2", 1234, true);
     // Save keys & values into ini file
     info.saveConfigFile();
   }else{ // Ini file is valid, display the values
-    LOG_I("Info file: var1: '%s', var2: '%s'\n", info["var1"].c_str(), info["var2"].c_str() );      
+    LOG_I("Config file already exists\n");
+    LOG_I("Info file: var1: '%s', var2: '%s'\n", info["var1"].c_str(), info["var2"].c_str() );
   }
 }
 
