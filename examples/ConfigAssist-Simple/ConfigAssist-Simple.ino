5af57b7526ff57d7f91d1bd375e5fe6ed0b2c63b
gemi254 <gemi254@192.168.56.1>
2024-11-19 15:17:55 +0200

    operators [], ()

--- examples/ConfigAssist-Simple/ConfigAssist-Simple.ino
+++ examples/ConfigAssist-Simple/ConfigAssist-Simple.ino
@@ -1,4 +1,5 @@
 #include <ConfigAssist.h>  // Config assist class
+bool reset = false;
 
 void setup() {
   // put your setup code here, to run once:
@@ -7,32 +8,35 @@ void setup() {
   Serial.print("\n\n\n\n");
   Serial.flush();
 
-  #if defined(ESP32)
-    if(!STORAGE.begin(true)) Serial.println("ESP32 storage init failed!");
-  #else
-    if(!STORAGE.begin()) Serial.println("ESP8266 storage init failed!");
-  #endif
   LOG_I("Starting..\n");
 
   // Create a config class with an ini filename for storage
-  // and disabled yaml (NULL)
+  // and disabled yaml dictionary (NULL)
+  // If ini file exists load it
   ConfigAssist info("/info.ini", NULL);
 
-  //info.deleteConfig(); // Uncomment to remove ini file and re-built
-
+  if(reset){
+    info.deleteConfig(); // Remove ini file and re-built
+    info.clear();        // Clear loaded keys
+  }
   // Ini file not exists
   if(!info.confExists()) {
     // Add a key even if not exists.
     // It will be not editable, but it will be saved to an ini file
     LOG_I("Config file not exists\n");
-    info.put("var1", "test1", true);
-    info.put("var2", 1234, true);
-    // Save keys & values into ini file
-    info.saveConfigFile();
+    info["string"] = "This is a string";
+    info["numString"] = "12345";
+    info["integer"] = 1;
+    info["float"] = 1.0;
+
   }else{ // Ini file is valid, display the values
     LOG_I("Config file already exists\n");
-    LOG_I("Info file: var1: '%s', var2: '%s'\n", info["var1"].c_str(), info["var2"].c_str() );
+    info["integer"] = info("integer").toInt() + 1;
+    info["float"] = info("float").toFloat() + .5;
   }
+  // Save keys & values into an ini file
+  info.saveConfigFile();
+  info.dump();
 }
 
 void loop() {
