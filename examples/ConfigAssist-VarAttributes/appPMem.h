18cec79dbc8a5d34d6629f2d3d8f0fa6cf0a5275
gemi254 <gemi254@192.168.56.1>
2024-11-19 15:18:39 +0200

    Fixed refresh

--- examples/ConfigAssist-VarAttributes/appPMem.h
+++ examples/ConfigAssist-VarAttributes/appPMem.h
@@ -30,34 +30,49 @@ Batch upload settings:
   - batch_upload:
       label: >-
         Enable batch upload measurements
-        <b>Upload interval &#8282;</b> <span id='uploadInterval'>2.5 mins</span>
+        <b>Upload interval &#8282;</b> <span id='uploadInterval'> 1.0 mins</span>
       checked: True
       attribs: onClick="enableDisableBatchUpload()"
+
   - batch_size:
       label: "Measurements size. Change to calculate <b>Upload interval</b>"
       default: 5
       attribs: >-
-        onChange="const ctrl = document.getElementById('update_interval');
-        const ui = ((ctrl.value * this.value) / 1000) / 60;
-        document.getElementById('uploadInterval').innerHTML = ui.toFixed(2) + ' mins';"
+        min='1' max='35535' step='1'
+        onChange="
+        const ctrl = document.getElementById('update_interval');
+        const batch = document.getElementById('batch_upload');
+        var ui = 0;
+        if(batch.checked)
+          ui = ( (ctrl.value/1000) * this.value ) / 60;
+        else
+          ui = ( (ctrl.value/1000) * 1 ) / 60;
+        document.getElementById('uploadInterval').innerHTML =  ui.toFixed(2)  + ' mins';
+        "
   - update_interval:
       label: "Delay between measurements (ms).Change to calculate <b>Upload interval</b>"
       default: 30000
       attribs: >-
-        onChange="const ctrl = document.getElementById('batch_size');
-        const ui = ((ctrl.value * this.value) / 1000) / 60;
-        document.getElementById('uploadInterval').innerHTML = ui.toFixed(2) + ' mins';"
+        onChange="
+          const ctrl = document.getElementById('batch_size');          
+          const batch = document.getElementById('batch_upload');
+          var ui = 0;
+          if(batch.checked)
+            ui = (ctrl.value * (this.value / 1000) ) / 60;
+          else 
+            ui = (1 * (this.value / 1000) ) / 60;
+          document.getElementById('uploadInterval').innerHTML = ui.toFixed(2) + ' mins';"
+        "
 )~";
 
 PROGMEM const char INIT_SCRIPT[] = R"=====(
 function enableDisableBatchUpload(){
-  if($('#batch_upload').checked){
+  if( document.getElementById('batch_upload').checked ){
     document.getElementById('batch_size').disabled = false;
-    document.getElementById('update_interval').disabled = false;
   }else{
-    document.getElementById('batch_size').disabled = true;
-    document.getElementById('update_interval').disabled = true;
+    document.getElementById('batch_size').disabled = true;   
   }
+  document.getElementById('batch_size').dispatchEvent(new Event('change'))
 }
 document.addEventListener('DOMContentLoaded', function (event) {
   enableDisableBatchUpload();
